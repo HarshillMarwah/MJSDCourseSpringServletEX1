1365cd7d100a8236c45e2c2c765a1174a12fe38e
Harshil IJ <marwahharshil@gmail.com>
2025-01-02 19:18:50 +0530

    Sending Basic response from SERVLET.

--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -3,10 +3,22 @@ package org.example;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res)
-    {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/HTML");
+        PrintWriter printWriter = res.getWriter();
+        printWriter.println("<h2><b>Hii from doGET </b> </h2>");
         System.out.println("Hi from Servlet");
     }
+    /*public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/HTML");
+        PrintWriter printWriter = res.getWriter();
+        printWriter.println("Hii from Service");
+        System.out.println("Hi from Servlet");
+    }*/
+
 }
